722281beb49a0bacac1562a13a74db3a365d411e
luis patino <lcp.patino@gmail.com>
2023-05-28 18:38:37 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(arr, target) {
+  // Create an empty set to store the visited numbers
+  const visited = new Set();
+
+  // Iterate through each number in the array
+  for (let num of arr) {
+    // Calculate the difference between the target and the current number
+    const diff = target - num;
+
+    // Check if the difference (required complement) has been visited before
+    if (visited.has(diff)) {
+      // If the complement exists in the set, we found a pair that adds up to the target
+      return true;
+    }
+
+    // Add the current number to the set of visited numbers
+    visited.add(num);
+  }
+
+  // If no pair is found, return false
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
